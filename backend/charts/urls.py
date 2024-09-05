332b23bc7a19ff461eddb27569ca24fd71dadb81
sriramreddy42 <49777806+sriramreddy42@users.noreply.github.com>
2024-09-05 03:07:41 -0400

    Delete backend directory

--- backend/charts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('charts/candlestick/', views.candlestick_data, name='candlestick_data'),
-    path('charts/line/', views.line_chart_data, name='line_chart_data'),
-    path('charts/bar/', views.bar_chart_data, name='bar_chart_data'),
-    path('charts/pie/', views.pie_chart_data, name='pie_chart_data'),
-]
